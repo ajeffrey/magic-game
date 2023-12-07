7950bd9865d7ef9edd1330509a77a02b08bc3856
Alex Jeffrey <alex@alexjeffrey.me>
2023-12-07 00:47:58 +0000

    fixed up vite

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
-import * as THREE from 'three';
-import Stats = require('stats.js');
-import { createRenderer, createScene } from './init';
-import { ModelLoader } from './loaders/ModelLoader';
-import { LevelLoader } from './loaders/LevelLoader';
-import { NavMesh } from './navigation/NavMesh';
-import { Player } from './Player';
-import { levelData, tileset } from './map';
-import { Rat } from './Rat';
-import { World } from './World';
+import * as THREE from "three";
+import Stats from "stats.js";
+import { createRenderer, createScene } from "./init";
+import { ModelLoader } from "./loaders/ModelLoader";
+import { LevelLoader } from "./loaders/LevelLoader";
+import { NavMesh } from "./navigation/NavMesh";
+import { Player } from "./Player";
+import { levelData, tileset } from "./map";
+import { Rat } from "./Rat";
+import { World } from "./World";
 
 const stats = new Stats();
 stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
 document.body.appendChild(stats.dom);
 
 const modelLoader = new ModelLoader();
-const modelNames = new Set(Object.values(tileset.tiles).map(o => o.m));
+const modelNames = new Set(Object.values(tileset.tiles).map((o) => o.m));
 
-modelLoader.loadGLTF('player', 'models/player.glb');
-modelLoader.loadGLTF('rat', 'models/rat.glb');
+modelLoader.loadGLTF("player", "models/player.glb");
+modelLoader.loadGLTF("rat", "models/rat.glb");
 
-for(const modelName of modelNames) {
+for (const modelName of modelNames) {
   modelLoader.loadGLTF(modelName, `models/${modelName}.glb`);
 }
 
-modelLoader.onReady(models => {
+modelLoader.onReady((models) => {
   const world = new World();
   const renderer = createRenderer();
   const scene = createScene();
@@ -31,54 +31,53 @@ modelLoader.onReady(models => {
   world.addEntity(navmesh);
   const levelLoader = new LevelLoader(tileset, models, navmesh);
 
-  for(const levelObj of levelData) {
+  for (const levelObj of levelData) {
     const level = levelLoader.load(levelObj);
     world.addEntity(level);
     scene.add(level.obj);
   }
-  
+
   // scene.add(navmesh.debug());
 
-  const player = new Player(models.get('player'));
+  const player = new Player(models.get("player"));
   world.addEntity(player);
   scene.add(player.model);
 
-  const rat = new Rat(models.get('rat'), new THREE.Vector3(3, -1, 3));
+  const rat = new Rat(models.get("rat"), new THREE.Vector3(3, -1, 3));
   world.addEntity(rat);
   scene.add(rat.model);
 
-  const rat2 = new Rat(models.get('rat'), new THREE.Vector3(1, -1, 3));
+  const rat2 = new Rat(models.get("rat"), new THREE.Vector3(1, -1, 3));
   world.addEntity(rat2);
   scene.add(rat2.model);
 
   let blurred = false;
 
-  window.addEventListener('blur', () => {
+  window.addEventListener("blur", () => {
     blurred = true;
-    console.log('lost window focus...');
+    console.log("lost window focus...");
   });
 
-  window.addEventListener('focus', () => {
+  window.addEventListener("focus", () => {
     blurred = false;
-    console.log('regained window focus...');
-  })
-  
+    console.log("regained window focus...");
+  });
+
   let prevTime = performance.now();
   let dt: number;
   function step(t) {
     stats.begin();
     dt = (t - prevTime) / 1000;
     prevTime = t;
-    
-    if(!blurred) {
+
+    if (!blurred) {
       world.update(dt);
       renderer.render(scene, player.camera.camera);
     }
-    
+
     stats.end();
-      requestAnimationFrame(step);
+    requestAnimationFrame(step);
   }
-  
+
   requestAnimationFrame(step);
-  
 });
--- src/loaders/ModelLoader.ts
+++ src/loaders/ModelLoader.ts
@@ -1,25 +1,25 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader';
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
-import SkeletonUtils = require("three/examples/jsm/utils/SkeletonUtils");
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { ColladaLoader } from "three/examples/jsm/loaders/ColladaLoader";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { clone } from "three/examples/jsm/utils/SkeletonUtils";
 
 const gltfLoader = new GLTFLoader();
 const colladaLoader = new ColladaLoader();
 const fbxLoader = new FBXLoader();
 
 export class ModelCollection {
-  private models: {[key: string]: THREE.Object3D};
+  private models: { [key: string]: THREE.Object3D };
 
   constructor() {
     this.models = {};
   }
 
   get(name: string): THREE.Object3D {
-    if(name in this.models) {
-      const skele = (SkeletonUtils as any).clone(this.models[name]);
-      skele.traverse(obj => {
-        if(obj instanceof THREE.Mesh) {
+    if (name in this.models) {
+      const skele = clone(this.models[name]);
+      skele.traverse((obj) => {
+        if (obj instanceof THREE.Mesh) {
           obj.castShadow = true;
           obj.receiveShadow = true;
           obj.material.format = THREE.RGBAFormat;
@@ -28,7 +28,6 @@ export class ModelCollection {
       });
 
       return skele;
-      
     } else {
       throw new Error(`model "${name}" not loaded`);
     }
@@ -49,51 +48,57 @@ export class ModelLoader {
   }
 
   loadGLTF(name: string, path: string) {
-    this.loaders.push(new Promise(resolve => {
-      gltfLoader.load(path, ({ scene }) => {
-        scene.traverse(obj => {
-          if(obj instanceof THREE.Mesh) {
-            obj.castShadow = true;
-            obj.receiveShadow = true;
-          }
+    this.loaders.push(
+      new Promise((resolve) => {
+        gltfLoader.load(path, ({ scene }) => {
+          scene.traverse((obj) => {
+            if (obj instanceof THREE.Mesh) {
+              obj.castShadow = true;
+              obj.receiveShadow = true;
+            }
+          });
+
+          this.models.add(name, scene);
+          resolve(null);
         });
-        
-        this.models.add(name, scene);
-        resolve(null);
-      });
-    }));
+      })
+    );
   }
 
   loadFBX(name: string, path: string, animations: string[] = []) {
-    this.loaders.push(new Promise(resolve => {
-      fbxLoader.load(path, (group) => {
-        group.traverse(obj => {
-          if(obj instanceof THREE.Mesh) {
-            obj.castShadow = true;
-            obj.receiveShadow = true;
-          }
-        });
+    this.loaders.push(
+      new Promise((resolve) => {
+        fbxLoader.load(path, (group) => {
+          group.traverse((obj) => {
+            if (obj instanceof THREE.Mesh) {
+              obj.castShadow = true;
+              obj.receiveShadow = true;
+            }
+          });
 
-        this.models.add(name, group);
-        resolve(null);
-      });
-    }));
+          this.models.add(name, group);
+          resolve(null);
+        });
+      })
+    );
   }
 
   loadCollada(name: string, path: string) {
-    this.loaders.push(new Promise(resolve => {
-      colladaLoader.load(path, ({ scene }) => {
-        scene.traverse(obj => {
-          if(obj instanceof THREE.Mesh) {
-            obj.castShadow = true;
-            obj.receiveShadow = true;
-          }
-        });
+    this.loaders.push(
+      new Promise((resolve) => {
+        colladaLoader.load(path, ({ scene }) => {
+          scene.traverse((obj) => {
+            if (obj instanceof THREE.Mesh) {
+              obj.castShadow = true;
+              obj.receiveShadow = true;
+            }
+          });
 
-        this.models.add(name, scene);
-        resolve(null);
-      });
-    }));
+          this.models.add(name, scene);
+          resolve(null);
+        });
+      })
+    );
   }
 
   onReady(cb: (models: ModelCollection) => any) {
